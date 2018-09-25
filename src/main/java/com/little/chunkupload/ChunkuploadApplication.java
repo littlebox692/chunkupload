49fbca77e573a9d3206b3b92f5e5e4f92f8a155e
littlebox692 <hwgs420086157@163.com>
2018-09-25 22:43:44 +0800

    create spring-boot template for this project

---
+++ src/main/java/com/little/chunkupload/ChunkuploadApplication.java
@@ -0,0 +1,12 @@
+package com.little.chunkupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunkuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunkuploadApplication.class, args);
+	}
+}
